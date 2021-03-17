a3756521d717d99a57894578040c2be7437eb16e
MetaSeth <alexandre.mokni@gmail.com>
2021-03-17 13:11:44 +0100

    clean code

--- app.ts
+++ app.ts
@@ -21,24 +21,27 @@ interface CoinDeskData {
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors())
-app.get('/btc', (_req, res) => {
-    getBtcPrice(24).then((snapshot) => {
+app.get('/btc', (req, res) => {
+    const hour = (parseInt(req.query.hour as string, 10) || 24);
+    const currency = req.query.currency as string || 'EUR';
+    getBtcPrice(hour).then((snapshot) => {
         const btcPrice: string[] = new Array();
         snapshot.forEach(
-            (childSnapshot) => { btcPrice.push(childSnapshot.val().bpi['EUR'].rate_float) }
+            (childSnapshot) => { btcPrice.push(childSnapshot.val().bpi[currency].rate_float) }
         )
         res.json(btcPrice);
     })
 });
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
-})
+});
 
 const options = {
     host: 'api.coindesk.com',
     path: '/v1/bpi/currentprice.json'
 };
 
+// Remove for production case
 const firebaseConfig = {
     apiKey: "AIzaSyDeMzqrRwhIg2jtSPEAYi5Wbpq43orxRkY",
     authDomain: "kwotdemoapp.firebaseapp.com",
@@ -52,21 +55,13 @@ const firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 const btcPriceListRef = firebase.database().ref('btcPrice');
 
-async function getBtcPrice(sinceHour: number, currency: string = "EUR") {
-    console.log("SomeOne ask btc price !");
+async function getBtcPrice(sinceHour: number) {
     const btcLastPrices = btcPriceListRef.limitToLast(sinceHour * 12);
-    return btcLastPrices.once('value', /*(snapshot) => {
-        const btcPrice: string[] = new Array();
-        snapshot.forEach(
-            (childSnapshot) => { btcPrice.push(childSnapshot.val().bpi[currency].rate) }
-        )
-        return btcPrice;*/
-        () => { }
-    );
+    btcPriceListRef.on()
+    return btcLastPrices.once('value');
 }
 
 const saveResponse = (response: any) => {
-
     response.on('data', (msg: any) => {
         const data: CoinDeskData = JSON.parse(msg.toString());
         btcPriceListRef.push().set(
@@ -77,10 +72,10 @@ const saveResponse = (response: any) => {
             (error: any) => {
                 if (error) {
                     // The write failed...
-                    console.log("Failed with error: " + error);
+                    console.log("Write failed with error: " + error);
                 } else {
                     // The write was successful...
-                    console.log("success");
+                    console.log("Write success");
                 }
             }
         )
@@ -92,17 +87,7 @@ function refresh() {
     http.request(options, saveResponse).end();
     setTimeout(refresh, 300000);
 }
-function test() {
-    getBtcPrice(24, 'EUR').then((snapshot) => {
-        const btcPrice: string[] = new Array();
-        snapshot.forEach(
-            (childSnapshot) => { btcPrice.push(childSnapshot.val().bpi['EUR'].rate) }
-        )
-        console.log(btcPrice);
-    })
-}
-test();
 // initialyze the regular polling
-setTimeout(refresh, 300000);
+// setTimeout(refresh, 300000);
 
 
